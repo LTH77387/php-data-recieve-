d5ed4c8b2e13aa3c6e713c76564e050af82324bb
Linn Thit <linnthit77387@gmail.com>
2022-11-20 07:45:48 +0630

    encode and decode with php

--- index.php
+++ index.php
@@ -3,19 +3,21 @@ $connect=mysqli_connect('localhost','root','','test_db');
 if(!$connect){
    die("Query Fail");
 }
-$name=$_POST['name'];
-$email=$_POST['email'];
-$phone=$_POST['phone'];
-$password=$_POST['password'];
-if($name == "" || $name==null && $email=="" || $email==null && $phone=="" || $phone==null && $password=="" || $password==null){
-    return "error";
+$encodeData=file_get_contents('php://input');
+$decodeData=json_decode($encodeData,true);
+$name=$decodeData['name'];
+$email=$decodeData['email'];
+$phone=$decodeData['phone'];
+$password=$decodeData['password'];
+$query="INSERT INTO users (name,email,phone,password) VALUES ('$name','$email','$phone','$password')";
+$result=mysqli_query($connect,$query);
+if($result){
+    $msg="Registered Successfully!";
 }else{
-    $query="INSERT INTO users (name,email,phone,password) VALUES ('$name','$email','$phone','$password')";
-    $result=mysqli_query($connect,$query);
-    if(!$result){
-        die("Query Fail");
-    }
+    $msg="Please try again....";
 }
+$res[]=array("Message:"=>$msg);
+echo json_encode($res);
 
 
 
